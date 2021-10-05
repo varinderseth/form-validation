90da3636c4a8208254f3745a051079fc3a91de28
Vipul Gupta <55375170+vipul0425@users.noreply.github.com>
2021-10-05 09:45:19 +0530

    added email validation

--- app.js
+++ app.js
@@ -8,7 +8,16 @@ function validateName() {
     const re = /^[a-zA-Z]{2,10}$/
     validate(name.value,re,name);
 }
-//emal
+function validateEmail() {
+    const email = document.getElementById('email');
+    const re = /^([a-zA-Z0-9_\.\~\-]+)@([a-zA-Z0-9_\.\~\-]+)\.[a-zA-Z]{2,5}$/
+    validate(email.value,re,email);
+}
+function validateZipcode() {
+    const zipcode = document.getElementById('zipcode');
+    const re = /^[1-9]{1}[0-9]{2}\s{0,1}?[0-9]{3}$/
+    validate(zipcode.value,re,zipcode);
+}
 function validateMobile() {
     const mobile = document.getElementById('mobile');
     const re = /^[\d]{10}$/
